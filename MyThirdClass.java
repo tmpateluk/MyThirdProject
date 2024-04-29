5255d88348b4949954cab240a9f1061816607d22
Tushar Patel <tmpateluk@gmail.com>
2024-04-29 21:18:12 +0100

    Submitting Third Project

---
+++ MyThirdClass.java
@@ -0,0 +1,9 @@
+package MyThirdPackage;
+
+public class MyThirdClass {
+    public static void main(String[] args) {
+        System.out.println("My third project");
+        System.out.println("Testing Techniques");
+        System.out.println("SDLC");
+    }
+}
