10f18db755aa4357341c3a689b2d70d020589723
Tushar Patel <tmpateluk@gmail.com>
2024-04-29 21:42:17 +0100

    Modified Project deleted line

--- MyThirdClass.java
+++ MyThirdClass.java
@@ -4,7 +4,6 @@ public class MyThirdClass {
     public static void main(String[] args) {
         System.out.println("My third project");
         System.out.println("Testing Techniques");
-        System.out.println("SDLC");
         System.out.println("Git Hub Addition");
     }
 }
